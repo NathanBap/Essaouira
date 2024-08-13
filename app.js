9372b3da23c364038f29f13e6d50853560435417
Nathan <bapin.nathan@gmail.com>
2024-08-13 11:57:26 +0200

    listen to PORT

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.disable('x-powered-by')
 
 app.use('/', router);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
