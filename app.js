0f7d241a0680f441240775c08bdde07e48bbadc1
Nathan <bapin.nathan@gmail.com>
2024-08-13 12:52:45 +0200

    Limite de 2 POST de formulaire toutes les 15 minutes

--- app.js
+++ app.js
@@ -14,8 +14,12 @@ app.disable('x-powered-by')
 
 app.use('/', router);
 
+
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import path from 'path';
 import * as formController from '../controllers/formControler.js';
+import rateLimit from 'express-rate-limit';
 
 const router = express.Router();
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // Fenêtre de 15 minutes
+    max: 2, // Limite chaque IP à 2 requêtes POST par fenêtre
+    message: "Vous avez soumis le formulaire trop de fois, veuillez réessayer plus tard"
+});
 
 router.route('/')
     .get((req, res) => { res.sendFile(path.join(process.cwd(), 'views', 'accueil.html')); });
 
 router.route('/contact')
     .get(formController.showForm)
-    .post(formController.createContact);
+    .post(limiter, formController.createContact);
 
 
 
