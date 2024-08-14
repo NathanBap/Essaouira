a721d002d6cc3277e2fa4536b440b4c3fc0f1905
Nathan <bapin.nathan@gmail.com>
2024-08-14 23:06:20 +0200

    added content into page

--- public/javascript/accueil.js
+++ public/javascript/accueil.js
@@ -4,15 +4,21 @@ document.getElementById('play-video').addEventListener('click', function() {
     var serviceVideo = document.getElementById('service-video');
     var videoContainer = document.getElementById('video-container');
     var content = document.getElementById('content');
+    var grid = document.getElementById('grid-container');
     
     // Forcer le reflow
     videoContainer.style.transform = 'translateX(-50%)';
+    //document.body.style.overflow = 'auto';
     
     setTimeout(function() {
         serviceVideo.play(); // Jouer la vidéo des services
         content.classList.add('hidden'); // Cacher le contenu
         button.classList.add('hidden'); // Cacher le bouton
     }, 500); // Correspondre à la durée de la transition
+
+    setTimeout(function() {
+        grid.style.display = 'grid'; // Afficher le grid
+    }, 1000);
 });
 
 
